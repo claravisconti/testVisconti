ae1e4a099aec2359eea447dc0869badea2536f59
Maria Clara Visconti <claravisconti@hotmail.com>
2020-11-19 02:00:40 -0300

    cards

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+const indexController = require('../controllers/indexController');
+
+router.get('/', indexController.root);
 
 module.exports = router;
